bcf9f5d89db165011050ca8d83e5bd1d790b46f7
Bhargavpatil16 <142925880+Bhargavpatil16@users.noreply.github.com>
2023-08-23 15:46:49 +0530

    Add files via upload

---
+++ Conditional_Assignment 2/Predict3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int test = 0;
+    cout << "First character " << '1' << endl;
+    cout << "Second character " << (test ? 3 : '1') << endl;
+    return 0;
+}
+
+// THE OUTPUT FOR THE PROGRAM WILL BE "FIRST CHARACTER 1" AND "SECOND CHARACTER 1"//
